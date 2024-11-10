92c93d2f99a49ee01c3019eef1b2c9a1ad1aee0c
Giliane Rochat <giliane.rochat@students.unibe.ch>
2024-11-10 23:02:04 +0100

    Parameter identifiability updated

--- Notebooks/identifiability.jl
+++ Notebooks/identifiability.jl
@@ -3,22 +3,18 @@ using StructuralIdentifiability, ModelingToolkit
 # This script analyses the global identifiability of the parameters of the ODE system used to model RhoA dynamics
 
 # Define ODE system using MTK
-@variables t
+@independent_variables t
 D = Differential(t)
     
-@mtkmodel RhoA_KO_dynamics begin
+@mtkmodel RhoA_dynamics begin
 
     @parameters begin
-        Kinact_GEF
-        Kcat_aGEF
-        Km_aGEF
-        Kcat_FL
-        Km_FL
-        Kinact_GAP
-        Kcat_aGAP
-        Km_aGAP
-        GEF0
-        GAP0
+        Kon_GEF
+        Koff_GEF
+        Kon_GAP
+        Koff_GAP
+        Kon_Rho
+        Koff_Rho
     end
 
     @variables begin
@@ -29,38 +25,27 @@ D = Differential(t)
     end
     
     @equations begin
-        D(aGEF) ~ Kinact_GEF * (GEF0 - aGEF)
-        D(aGAP) ~ Kcat_FL * ((aRhoA-1)^2*(tanh(100*(aRhoA-1))-tanh(100*(aRhoA-10)))/2 * (10 - aGAP))/(Km_FL + (10 - aGAP)) + Kinact_GAP * (GAP0-aGAP)
-        #D(aGAP) ~ Kcat_FL * (aRhoA-1) + Kinact_GAP * (0.5-aGAP)
-        #D(aGAP) ~ Kcat_FL * ((10 - aGAP) * (aRhoA-1)^6)/(Km_FL + (10 - aGAP)) + Kinact_GAP * (0.5-aGAP)
-        D(aRhoA) ~ Kcat_aGEF * (aGEF-GEF0)*(tanh(100*(aGEF-GEF0))-tanh(100*(aGEF-10)))/2 * (10 - aRhoA)/(Km_aGEF + (10 - aRhoA)) - Kcat_aGAP * (aGAP-GAP0)*(tanh(100*(aGAP-GAP0))-tanh(100*(aGAP-10)))/2 * (aRhoA)/(Km_aGAP + (aRhoA))
-        #D(aRhoA) ~ Kcat_aGEF * (aGEF-0.5) - Kcat_aGAP * (aGAP-0.5)
-        #D(aRhoA) ~ Kcat_aGEF * (aGEF-0.5) * (10 - aRhoA)/(Km_aGEF + (10 - aRhoA)) - Kcat_aGAP * (aGAP-0.5) #* aRhoA/(Km_aGAP + aRhoA)
+        D(aGEF) ~ Kon_GEF - Koff_GEF * aGEF
+        D(aGAP) ~ Kon_GAP * (10 - aGAP) * aRhoA - Koff_GAP * aGAP
+        D(aRhoA) ~ Kon_Rho * aGEF * (10 - aRhoA) - Koff_Rho * aGAP * aRhoA
         y ~ aRhoA
     end
 end
 
-@mtkbuild RhoA_KO_model = RhoA_KO_dynamics()
+@mtkbuild RhoA_model = RhoA_dynamics()
 
 # Assess global identifiability without initial conditions
-global_identifiability = assess_identifiability(RhoA_KO_model, measured_quantities=[RhoA_KO_model.y ~ RhoA_KO_model.aRhoA], prob_threshold = 0.99)
+global_identifiability = assess_identifiability(RhoA_model, measured_quantities=[RhoA_model.y ~ RhoA_model.aRhoA], prob_threshold = 0.99)
+println("Parameter identifiability of RhoA model :")
 println(global_identifiability)
 
 
 # Define ODE system using ODEmodel to assess parameter identifiability with initial conditions
 RhoA_ode = @ODEmodel(
-    aGEF'(t) = Kinact_GEF * (0.5 - aGEF(t)),
-    aGAP'(t) ~ (Kcat_FL * ((aRhoA(t)-1)^2*(tanh(100*(aRhoA(t)-1))-tanh(100*(aRhoA(t)-10)))/2 * 
-            (10 - aGAP(t)))/(Km_FL + (10 - aGAP(t))) + Kinact_GAP * (0.5-aGAP(t)))
-
-    aRhoA'(t) = Kcat_aGEF * (aGEF(t)-0.5)*(tanh(100*(aGEF(t)-0.5))-tanh(100*(aGEF(t)-10)))/2 * (10 - aRhoA(t))/(Km_aGEF + (10 - aRhoA(t))) - Kcat_aGAP * (aGAP(t)-0.5)*(tanh(100*(aGAP(t)-0.5))-tanh(100*(aGAP(t)-10)))/2 * (aRhoA(t))/(Km_aGAP + (aRhoA(t)))
-    
+    aGEF'(t) = Kon_GEF - Koff_GEF * aGEF(t),
+    aGAP'(t) = Kon_GAP * (10 - aGAP(t)) * aRhoA(t) - Koff_GAP * aGAP(t),
+    aRhoA'(t) = Kon_Rho * aGEF(t) * (10 - aRhoA(t)) - Koff_Rho * aGAP(t) * aRhoA(t),
     y(t) = aRhoA(t)
-
-    #aGAP'(t) = Kcat_FL * ((10 - aGAP(t)) * (aRhoA(t)-1)^6)/(Km_FL + (10 - aGAP(t))) + Kinact_GAP * (0.5-aGAP(t)),
-    #aGAP'(t) = Kcat_FL * (aRhoA(t)-1)^6 + Kinact_GAP * (0.5 - aGAP(t)),
-    #aRhoA'(t) = Kcat_aGEF * (aGEF(t)-0.5) * (10 - aRhoA(t))/(Km_aGEF + (10 - aRhoA(t))) - (Kcat_aGAP * (aGAP(t)-0.5) * aRhoA(t)/(Km_aGAP + aRhoA(t))),
-    #aRhoA'(t) = Kcat_aGEF * (aGEF(t)-0.5) - (Kcat_aGAP * (aGAP(t)-0.5)),
 )
 
 # Assess global identifiability with initial conditions
